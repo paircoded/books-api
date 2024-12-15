39cff2bcfb0cf9fd0c89443125260fde84204e27
Tyler Evans <tyler@poorlythoughtout.com>
2024-12-15 13:03:48 -0800

    Add basic file upload route

--- books_api/main.py
+++ books_api/main.py
@@ -1,6 +1,7 @@
-from typing import Union
+from typing import Union, Annotated
 
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, File
+
 from fastapi.middleware.cors import CORSMiddleware
 
 from books_api.auth.dependencies import account_access_token
@@ -23,11 +24,14 @@ app.add_middleware(
 
 @app.get("/")
 def list_home_data():
-    return {
-
-    }
+    return {"hello": "world"}
 
 
 @app.get("/items/{item_id}", dependencies=[Depends(account_access_token)])
 def create_book(item_id: int, q: Union[str, None] = None):
     return {"books": item_id, "q": q}
+
+
+@app.get("/books/upload", dependencies=[Depends(account_access_token)])
+def upload_book(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
