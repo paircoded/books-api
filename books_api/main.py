d223e3661eda95725759d8562544328996f451c2
Tyler Evans <tyler@poorlythoughtout.com>
2024-12-15 11:00:11 -0800

    Add jwt auth

--- books_api/main.py
+++ books_api/main.py
@@ -1,8 +1,12 @@
-from typing import Union
+from typing import Union, Annotated
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2AuthorizationCodeBearer
 
+
+from jwt import PyJWKClient
+import jwt
 app = FastAPI()
 
 
@@ -21,12 +25,41 @@ app.add_middleware(
 )
 
 
+oauth_2_scheme = OAuth2AuthorizationCodeBearer(
+    tokenUrl="https://accounts.paircoded.com/realms/paircoded/protocol/openid-connect/token",
+    authorizationUrl="https://accounts.paircoded.com/realms/paircoded/protocol/openid-connect/auth",
+    refreshUrl="https://accounts.paircoded.com/realms/paircoded/protocol/openid-connect/token",
+)
+
+
+async def valid_access_token(
+    access_token: Annotated[str, Depends(oauth_2_scheme)]
+):
+    url = "http://path/to/realm/protocol/openid-connect/certs"
+    optional_custom_headers = {"User-agent": "custom-user-agent"}
+    jwks_client = PyJWKClient(url, headers=optional_custom_headers)
+
+    try:
+        signing_key = jwks_client.get_signing_key_from_jwt(access_token)
+        data = jwt.decode(
+            access_token,
+            signing_key.key,
+            algorithms=["RS256"],
+            audience="api",
+            options={"verify_exp": True},
+        )
+        return data
+    except jwt.exceptions.InvalidTokenError:
+        raise HTTPException(status_code=401, detail="Not authenticated")
+
+
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}", dependencies=[Depends(valid_access_token)])
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
