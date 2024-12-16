83d4b94d02c9d6c480bc9d219f348d55ee76bc0a
Tyler Evans <tyler@poorlythoughtout.com>
2024-12-15 20:42:19 -0800

    create book in db

--- books_api/main.py
+++ books_api/main.py
@@ -1,11 +1,19 @@
-from fastapi import FastAPI, Depends, UploadFile
+import hashlib
+import os
+import uuid
+from typing import Annotated
+
+from fastapi import FastAPI, Depends, UploadFile, status
 from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from books_api import services
 from books_api.auth.dependencies import account_access_token
-from books_api.persist.dependencies import get_db_session
+from books_api.persist import models
+from books_api.persist.dependencies import get_db_session, get_repository
+from books_api.persist.repository import DatabaseRepository
 from books_api.services import save_uploaded_book
+from books_api.settings import settings
 from books_api.types import PaginatedResultSet, Book
 
 app = FastAPI()
@@ -23,8 +31,13 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+BookRepository = Annotated[
+    DatabaseRepository[models.Book],
+    Depends(get_repository(models.Book)),
+]
 
-@app.get("/books", dependencies=[Depends(account_access_token)])
+
+@app.get("/books", dependencies=[Depends(account_access_token)], status_code=status.HTTP_201_CREATED)
 async def list_books(
         offset: int = 0,
         limit: int = 25,
@@ -33,10 +46,21 @@ async def list_books(
     return await services.list_books(db_session, offset=offset, limit=limit)
 
 
-
-@app.post("/books/upload", dependencies=[Depends(account_access_token)], response_model=Book, response_model_exclude={'path'})
+@app.post("/books/upload", dependencies=[Depends(account_access_token)], response_model=Book,
+          response_model_exclude={'path'})
 async def upload_book(
         file: UploadFile,
-        db_session: AsyncSession = Depends(get_db_session),
+        repository: BookRepository
 ):
-    return await save_uploaded_book(db_session, file)
+    filename_hash = hashlib.md5(file.filename.encode('utf-8')).hexdigest()
+    filename = f"{filename_hash}.epub"
+    output_file_path = os.path.join(settings.book_storage_base_directory, filename)
+    with open(output_file_path, "wb") as output_file:
+        output_file.write(file.file.read())
+    book = Book(
+        id=uuid.uuid4(),
+        title=file.filename,
+        path=output_file_path,
+    )
+
+    repository.create(book)
--- books_api/services.py
+++ books_api/services.py
@@ -5,6 +5,7 @@ import uuid
 from sqlalchemy import select
 
 from books_api.persist import models
+from books_api.persist.repository import DatabaseRepository
 from books_api.settings import settings
 from books_api.types import Book, PaginatedResultSet
 
@@ -15,15 +16,18 @@ async def list_books(db_session, offset=0, limit=25) -> PaginatedResultSet[Book]
     return PaginatedResultSet[Book](offset=offset, limit=limit, objects=books)
 
 
-async def save_uploaded_book(db_session, file) -> Book:
+async def save_uploaded_book(repository: DatabaseRepository[models.Book], file) -> Book:
     filename_hash = hashlib.md5(file.filename.encode('utf-8')).hexdigest()
     filename = f"{filename_hash}.epub"
     output_file_path = os.path.join(settings.book_storage_base_directory, filename)
     with open(output_file_path, "wb") as output_file:
         output_file.write(file.file.read())
 
-    return Book(
+    book = Book(
         id=uuid.uuid4(),
         title=file.filename,
         path=output_file_path,
     )
+
+    db_book = repository.create(book.model_dump())
+    return Book.model_validate(db_book)
